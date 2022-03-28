489edebf8aa7e0064e60cf50860a9f0da061cdc3
Tania Tasmin <taniat.kaau@email.com>
2022-03-28 14:51:29 -0400

    Add table users in database

--- backend/db/schema/users.sql
+++ backend/db/schema/users.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    city VARCHAR(255),
+    city VARCHAR(255)
+    
 );
