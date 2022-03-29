abadfde4970e3475a8f5bcad0529999ef705cd6e
Tania Tasmin <taniat.kaau@email.com>
2022-03-29 07:30:07 -0400

    Create seeds for table realtors

---
+++ backend/db/seeds/realtors.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+realtors(name,  phone, email, image, description, top_seller, date_hired)
+VALUES
+    ('Alec Quon', '416-293-4321', 'alec@quon.com', 'https://images.pexels.com/photos/1270076/pexels-photo-1270076.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 
+     'Awesome realtor with 15 years of experience in the field, has helped many people find
+      great deals and helped sell their homes', false, '2021-11-27 12:52:51.337466');
+   
+INSERT INTO
+realtors(name,  phone, email, image, description, top_seller, date_hired)
+VALUES
+    ('Sven Jones', '903-456-5612', 'sven@jones.com', 'https://images.pexels.com/photos/737586/pexels-photo-737586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     'Great realtor that has helped many individuals buy and sell home', false, '2021-08-19 12:52:51.337466');   
